8f88d4610b17cef73013ffdd2a529476be109d7d
dericfung <dericfung3028@hotmail.com>
2020-05-26 16:58:06 -0600

    Added js codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log("Hello! Welcome to my UX Design portfolio website!");
+
+function menuToggle() {
+}
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+}
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
