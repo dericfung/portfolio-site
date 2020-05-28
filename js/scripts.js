088befc13c3633e42fa4eb4a69bae31fd911b7a0
dericfung <dericfung3028@hotmail.com>
2020-05-27 20:20:14 -0600

    Added jQuery to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 console.log("Hello! Welcome to my UX Design portfolio website!");
 
 function menuToggle() {
-}
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-}
-function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
